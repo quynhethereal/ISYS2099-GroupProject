a9206b8bd6230ae89cc8eb20ddf7810f9de21247
quynhethereal <quynh.le@holistics.io>
2023-08-08 18:54:19 +0700

    Refactor to use Promised-based func

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,18 +1,20 @@
 const User = require("../models/user.model");
 
 // find user by username and password
-exports.findByUsernamePassword = (req, res) => {
-    User.findByUsernamePassword(
-        req.body.username,
-        req.body.password,
-        (err, user) => {
-            if (err) {
-                res.status(500).send({
-                    message: err.message || "Error retrieving user."
-                });
-            } else {
-                res.send(user);
-            }
+exports.findByUsernamePassword = async (req, res) => {
+    try {
+        const user = await User.findByUsernamePassword(req.body.username, req.body.password);
+
+        if (user) {
+            res.send(user);
+        } else {
+            res.status(401).send({
+                message: "Unauthorized"
+            });
         }
-    );
-}
+    } catch (err) {
+        res.status(500).send({
+            message: err.message || "Error retrieving user."
+        });
+    }
+};
