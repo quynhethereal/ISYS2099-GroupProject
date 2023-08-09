85b3703e915ed46648c94410ce9fc53271c9818c
quynhethereal <quynh.le@holistics.io>
2023-08-09 13:38:00 +0700

    Add CORS middleware

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,8 +1,8 @@
+
 module.exports = app => {
     const users = require("../controllers/user.controller.js");
     const auth = require("../controllers/auth.controller.js");
     const products = require("../controllers/product.controller.js");
-
     const authMiddleware = require('../middlewares/auth.middleware');
 
     let router = require("express").Router();
@@ -16,7 +16,7 @@ module.exports = app => {
     router.get("/user", authMiddleware.verifyToken, users.findAll);
 
     // product-related API
-    router.get("/products", products.findAll);
+    router.get("/products", authMiddleware.verifyToken, products.findAll);
 
 
     app.use('/api', router);
