ffcf743d5e577325c8434717822d6269df04ff58
DarcieNg <s3865443@rmit.edu.vn>
2023-08-09 14:40:29 +0700

    update for run

--- src/db/db.js
+++ src/db/db.js
@@ -1,107 +1,54 @@
 const mysql = require('mysql2');
 
-// Create a admin connection pool to the MySQL database
-const admin_pool = mysql.createPool ({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_ADMIN_USER,
-  password: process.env.MYSQL_ADMIN_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
-  waitForConnections: true,
-  connectionLimit: 10,
-  maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
-  idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
-  queueLimit: 0,
-  enableKeepAlive: true,
-  keepAliveInitialDelay: 0
-});
+// // Create a admin connection pool to the MySQL database
+// var admin_pool      =    mysql.createPool({
+//   connectionLimit : 10,
+//   host: process.env.MYSQL_HOST,
+//   user: process.env.MYSQL_ADMIN_USER,
+//   password : process.env.MYSQL_ADMIN_PASSWORD,
+//   database: process.env.MYSQL_DATABASE,
+//   debug    :  false
+// }); 
 
-// Connect admin to the database
-let queryString =  `SELECT * FROM ${process.env.MYSQL_HOST}`;
-admin_pool.query(queryString, (err) => {
-  if (err) {
-    console.error('Error connecting admin to MySQL:', err);
-    return;
-  }
-  console.log('Connected to MySQL database');
-});
+// admin_pool.getConnection(function(err,connection){
+//   if (err) {
+//     console.error('Error connecting admin to MySQL:', err);
+//     connection.release();
+//     throw err;
+//   }   
+
+//   console.log('Connected admin to MySQL:', err);
+//   connection.on('error', function(err) {      
+//         throw err;
+//         return;     
+//   });
+// });
 
 module.exports = admin_pool;
 
-// Connect customer to the database
-// Create a customer connection pool to the MySQL database
-const customer_pool = mysql.createPool ({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_CUSTOMER_USER,
-  password: process.env.MYSQL_CUSTOMER_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
-  waitForConnections: true,
-  connectionLimit: 10,
-  maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
-  idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
-  queueLimit: 0,
-  enableKeepAlive: true,
-  keepAliveInitialDelay: 0
+// Create a connection to the MySQL database
+const connection = mysql.createConnection({
+  host: process.env.MYSQL_HOST,       // MySQL server hostname
+  user: process.env.MYSQL_USERNAME,    // MySQL username
+  password: process.env.MYSQL_PASSWORD, // MySQL password
+  database: process.env.MYSQL_DATABASE // MySQL database name
 });
 
-customer_pool.query(queryString, (err) => {
+connection.connect((err) => {
   if (err) {
-    console.error('Error connecting customer to MySQL:', err);
+    console.error('Error connecting to MySQL:', err);
     return;
   }
   console.log('Connected to MySQL database');
 });
 
-module.exports = customer_pool;
-
-// Connect seller to the database
-// Create a customer connection pool to the MySQL database
-const seller_pool = mysql.createPool ({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_SELLER_USER,
-  password: process.env.MYSQL_SELLER_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
-  waitForConnections: true,
-  connectionLimit: 10,
-  maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
-  idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
-  queueLimit: 0,
-  enableKeepAlive: true,
-  keepAliveInitialDelay: 0
-});
-
-seller_pool.query(queryString, (err) => {
-  if (err) {
-    console.error('Error connecting seller to MySQL:', err);
-    return;
-  }
-  console.log('Connected to MySQL database');
-});
-
-module.exports = seller_pool;
-
-// // Create a connection to the MySQL database
-// const connection = mysql.createConnection({
-//   host: process.env.MYSQL_HOST,       // MySQL server hostname
-//   user: process.env.MYSQL_USERNAME,    // MySQL username
-//   password: process.env.MYSQL_PASSWORD, // MySQL password
-//   database: process.env.MYSQL_DATABASE // MySQL database name
-// });
-
-// connection.connect((err) => {
-//   if (err) {
-//     console.error('Error connecting to MySQL:', err);
-//     return;
-//   }
-//   console.log('Connected to MySQL database');
-// });
-
-// module.exports = connection;
+module.exports = connection;
 
 // Close the connection when done
-// connection.end((err) => {
-//   if (err) {
-//     console.error('Error closing MySQL connection:', err);
-//     return;
-//   }
-//   console.log('MySQL connection closed');
-// });
+connection.end((err) => {
+  if (err) {
+    console.error('Error closing MySQL connection:', err);
+    return;
+  }
+  console.log('MySQL connection closed');
+});
