a54e63ac4e29965ccff19661a3553e2bd3fa2aa3
DarcieNg <s3865443@rmit.edu.vn>
2023-08-09 17:27:44 +0700

    update pool

--- src/db/db.js
+++ src/db/db.js
@@ -1,78 +1,76 @@
-const mysql = require("mysql2");
+const mysql = require('mysql2');
 
 // Create a admin connection pool to the MySQL database
 const admin_pool = mysql.createPool({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_ADMIN_USER,
-  password: process.env.MYSQL_ADMIN_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
-  waitForConnections: true,
-  connectionLimit: 10,
-  maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
-  idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
-  queueLimit: 0,
-  enableKeepAlive: true,
-  keepAliveInitialDelay: 0,
+    connectionLimit: 10,
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_ADMIN_USER,
+    password: process.env.MYSQL_ADMIN_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    debug: false
 });
 
-// Connect admin to the database
-let queryString = `SELECT * FROM ${process.env.MYSQL_HOST}`;
-admin_pool.query(queryString, (err) => {
-  if (err) {
-    console.error("Error connecting admin to MySQL:", err);
-    return;
-  }
-  console.log("Connected to MySQL database");
+admin_pool.getConnection(function (err, connection) {
+    if (err) {
+        console.error('Error connecting admin to MySQL:', err);
+        connection.release();
+        throw err;
+    }
+
+    console.log('Connected admin to MySQL:', err);
+    connection.on('error', function (err) {
+        throw err;
+    });
 });
 
-// module.exports = admin_pool;
+module.exports = admin_pool;
 
-// Connect customer to the database
 // Create a customer connection pool to the MySQL database
 const customer_pool = mysql.createPool({
+  connectionLimit: 10,
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_CUSTOMER_USER,
   password: process.env.MYSQL_CUSTOMER_PASSWORD,
   database: process.env.MYSQL_DATABASE,
-  waitForConnections: true,
-  connectionLimit: 10,
-  maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
-  idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
-  queueLimit: 0,
-  enableKeepAlive: true,
-  keepAliveInitialDelay: 0,
+  debug: false
 });
 
-customer_pool.query(queryString, (err) => {
+customer_pool.getConnection(function (err, connection) {
   if (err) {
-    console.error("Error connecting customer to MySQL:", err);
-    return;
+      console.error('Error connecting customer to MySQL:', err);
+      connection.release();
+      throw err;
   }
-  console.log("Connected to MySQL database");
+
+  console.log('Connected customer to MySQL:', err);
+  connection.on('error', function (err) {
+      throw err;
+  });
 });
 
 module.exports = customer_pool;
 
-// Connect seller to the database
-// Create a customer connection pool to the MySQL database
+// Create a seller connection pool to the MySQL database
 const seller_pool = mysql.createPool({
+  connectionLimit: 10,
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_SELLER_USER,
   password: process.env.MYSQL_SELLER_PASSWORD,
   database: process.env.MYSQL_DATABASE,
-  waitForConnections: true,
-  connectionLimit: 10,
-  maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
-  idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
-  queueLimit: 0,
-  enableKeepAlive: true,
-  keepAliveInitialDelay: 0,
+  debug: false
 });
 
-seller_pool.query(queryString, (err) => {
+seller_pool.getConnection(function (err, connection) {
   if (err) {
-    console.error("Error connecting seller to MySQL:", err);
-    return;
+      console.error('Error connecting seller to MySQL:', err);
+      connection.release();
+      throw err;
   }
-  console.log("Connected to MySQL database");
+
+  console.log('Connected seller to MySQL:', err);
+  connection.on('error', function (err) {
+      throw err;
+  });
 });
+
+module.exports = seller_pool;
