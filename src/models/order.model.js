e923ff442983f857fa24e3e3361c5f6bb8a915e8
quynhethereal <quynh.le@holistics.io>
2023-08-13 15:01:19 +0700

    Modify get orders API

--- src/models/order.model.js
+++ src/models/order.model.js
@@ -1,18 +1,13 @@
 const {customer_pool} = require("../db/db");
 
-const OrderStatus = {
-    CREATED: 'created',
-    ACCEPT: 'accept',
-    CANCEL: 'cancel',
-};
+
 
 class Order {
     constructor(params = {}) {
         this.id = params.id;
         this.userId = params.user_id;
-        this.totalPrice = 0;
-        this.status = OrderStatus.CREATED;
-        this.orderItems = [];
+        this.totalPrice = params.total_price;
+        this.status = params.status;
     }
 }
 
