a14040954f1f26651702e192bba127b43065a286
quynhethereal <quynh.le@holistics.io>
2023-08-14 14:23:31 +0700

    Fix findAll API'

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -3,7 +3,7 @@ const Helper = require('../helpers/helpers');
 
 exports.findAll = async (req, res) => {
     try {
-        const products = await Product.findAll(req.params);
+        const products = await Product.findAll(req.query);
         res.status(200).json(products);
     } catch (err) {
         res.status(500).send({
