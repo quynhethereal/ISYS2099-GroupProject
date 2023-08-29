4fad291cc946064eb175c88406ff98ae64f8975b
Kisari <seikakukisari@gmail.com>
2023-08-29 11:42:31 +0700

    add sellers page + control panel

---
+++ src/frontend/src/components/seller/SellerProductList.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const SellerProductList = () => {
+  return (
+    <div className="container p-4 d-flex flex-column justify-content-start align-items-center"></div>
+  );
+};
+
+export default SellerProductList;
