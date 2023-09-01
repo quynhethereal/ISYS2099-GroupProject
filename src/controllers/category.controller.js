505b6c8a735bf4ce19f2129b65772cd2b40f041e
DarcieNg <s3865443@rmit.edu.vn>
2023-09-01 22:37:43 +0700

    delete unnecessary

--- src/controllers/category.controller.js
+++ src/controllers/category.controller.js
@@ -2,43 +2,6 @@ const {Category} = require('../models/category.model');
 const {CategoryTree} = require('../helpers/CategoryTree');
 const Product = require('../models/product.model');
 
-/*
-body params 
-{
-    name: New Category
-    attributes: [
-        descriptions: ["abc","xyz", 123]
-    ], 
-    subcategories: [
-        name: New Category
-        attributes: [
-            descriptions: ["a1b","txd", 234]
-        ], 
-    ]
-}
-*/
-exports.createCategory = async (req, res) => {
-    try {
-        const name = req.body.name;
-        const duplicateCategory = await Category.findOne({name: name});
-        if (duplicateCategory) {
-            res.status(400).send({
-                message: "Invalid request. Category is existed."
-            });
-            return;
-        } 
-
-        // Handle generate ID
-
-        const category = Category.create(req.body);
-        res.status(200).json(category);
-    } catch (err) {
-        res.status(500).send({
-            message: err.message || "Error creating category."
-        });
-    }   
-}
-
 const findAll = async () => {
     try {
         const categories = await Category.find({});
@@ -66,59 +29,6 @@ exports.findAll = async (req, res) => {
     } catch (err) {
         res.status(500).send({
             message: err.message || "Error finding products by key."
-        });
-    }
-}
-
-const findNestedSubcategories = async (category) => {
-    const catObj = {
-        id: category.id,
-        parentId: category.parentId,
-        name: category.name
-    };
-
-    const data = [];
-    data.push(catObj);
-
-    if (category.subcategories && category.subcategories.length > 0) {
-        
-        const subcategories = await Promise.all(category.subcategories.map((subcategory) => (findNestedSubcategories(subcategory))));
-
-        for (const subcategory of subcategories) {
-            data.push(...subcategory);
-        }
-    }
-    console.log(data);
-
-    return data;
-}
-
-const findAllCatAndSubCat = async () => {
-    try {
-        const categories = await Category.find({});
-        const data = [];
-
-        for (const category of categories) {
-            const catObj = await findNestedSubcategories(category);
-            data.push(...catObj);
-        }
-
-        return data;
-    } catch (err) {
-        throw new Error("Error fetching id and name of categories and subcategories.", err);
-    }
-}
-
-// Get both categories and subcategories at same level
-exports.findAllSameLevels = async (req, res) => {
-    try {
-        const data = await findAllCatAndSubCat();
-
-        res.status(200).json(data);
-
-    } catch (err) {
-        res.status(500).send({
-            message: err.message || "Error getting categories and subcategories as list for user."
         });
     }
 }
@@ -330,91 +240,6 @@ exports.findAttributesProduct = async (req, res) => {
     } catch (err) {
         res.status(500).send({
             message: err.message || "Error adding attributes to category."
-        });
-    }
-}
-
-exports.update = async (req, res) => {
-    try {
-        const id = parseInt(req.params.id);
-        const name = req.body.name;
-        const attributes = req.body.attributes;
-        var parent = req.body.parent; // optional attribute
-
-        if (name == null || attributes == null) {
-            res.status(400).send({
-                message: "Invalid request."
-            });
-            return;
-        }
-
-        // Check value name is existed
-        const originalCategory = await Category.findOne({id: id});
-        const findCategory = await Category.findOne({name: name});
-
-        // Check if the name of the category is duplicated with others.
-        if (name != originalCategory.name && findCategory) {
-            res.status(400).send({
-                message: "Invalid request. Category is existed."
-            });
-            return;
-        }
-
-        // Check if the category contains parent category
-        if (parent == null) {
-            // Put update value in params
-            const params = {
-                name: name,
-                attributes: attributes
-            }
-
-            // find and update category
-            var updateCategory = await Category.findOneAndUpdate({id: id}, params, {new: true});
-            res.status(200).json(updateCategory);
-        } else {
-            const parentId = parseInt(parent);
-
-            // Check if the parent ID is not the same as ID of category
-            if (!parentId || parentId == id) {
-                res.status(400).send({
-                    message: "Invalid parent ID."
-                });
-                return;
-            }
-
-            const params = {
-                name: name,
-                attributes: attributes,
-                parent: parentId
-            }
-    
-            var updateCategory = await Category.findOneAndUpdate({id: id}, params, {new: true});
-            res.status(200).json(updateCategory);
-        }
-        
-    } catch (err) {
-        res.status(500).send({
-            message: err.message || "Error updating category."
-        });
-    }
-}
-
-exports.delete = async (req, res) => {
-    try {
-        //TODO: Delete category
-        const count = await Product.countByCategory(req.params.id);
-
-        // TODO: Handle API after delete
-        if (count > 0) {
-            res.status(400).send({
-                message: "Invalid request. This category has some remained products. Could not delete."
-            })
-        }
-        Category.deleteOne({id: req.params.id});
-
-    } catch (err) {
-        res.status(500).send({
-            message: err.message || "Error deleting category."
         });
     }
 }
