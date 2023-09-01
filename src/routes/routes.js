a27a5873b46f1db978ead6089a819cf6f9dd7284
DarcieNg <s3865443@rmit.edu.vn>
2023-09-01 14:55:26 +0700

    update attributes list to set

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -70,12 +70,12 @@ module.exports = app => {
 
 
     // category api for testing
-    router.post("/category", categories.createCategory);
+    // router.post("/category", categories.createCategory);
     router.get("/category", categories.findAll);
     router.get("/category/customer", categories.findAllSameLevels);
     router.get("/category/:id", categories.findOne);
     router.get("/category/:id/attributes", categories.findAttributes);
-    router.put("/category/:id", categories.update);
+    // router.put("/category/:id", categories.update);
 
     app.use('/api', router);
 }
