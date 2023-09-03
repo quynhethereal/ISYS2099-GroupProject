ec7130a18901cbf95de9c709fb51a58917cf7a6f
Kisari <seikakukisari@gmail.com>
2023-09-03 14:09:39 +0700

    create + add attribute form

---
+++ src/frontend/src/utils/CategoryCreateForm.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+
+const CategoryCreateForm = ({ data, show, handleClose }) => {
+  return (
+    <Modal show={show} onHide={handleClose} size="lg">
+      <Modal.Header closeButton>
+        <Modal.Title className="ms-auto">Create Category </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <div className="d-flex flex-column justify-content-center align-items-center"></div>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button
+          className="w-100"
+          variant="primary"
+          onClick={() => handleClose()}
+        >
+          Ok
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+export default CategoryCreateForm;
