ec595b3fb8100aa54ed171c18c484939953b0aef
quynhethereal <quynh.le@holistics.io>
2023-09-03 16:36:16 +0700

    Modify customer permission

--- script/seed.sql
+++ script/seed.sql
@@ -233,8 +233,8 @@ GRANT ALL PRIVILEGES ON lazada_ecommerce.* TO 'admin';
 grant select on lazada_ecommerce.products to 'customer';
 grant insert, select, update on lazada_ecommerce.users_info to 'customer';
 grant insert, select, update on lazada_ecommerce.orders to 'customer';
--- grant insert, select, update, delete on lazada_ecommerce.order_items to 'customer';
--- grant select on lazada_ecommerce.inventory to 'customer';
+grant select on lazada_ecommerce.order_items to 'customer';
+grant select on lazada_ecommerce.inventory to 'customer';
 -- grant execute on procedure lazada_ecommerce.UPDATE_INVENTORY_ON_ORDER_ACCEPT to 'customer';
 
 -- Seller: CRUD product, CRU users_info, orders and inventory
